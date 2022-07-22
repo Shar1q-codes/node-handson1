6e8e76c74639ce8656075c2a473c3bcf6d19b6bf
Shar1q-codes <shariqhussain691@gmail.com>
2022-07-22 19:22:16 +0530

    done

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require("http")
+
+const server = http.createServer((req,res)=>{
+    res.write("Hello World")
+    res.end()
+})
+
+server.listen(8000,()=>{
+    console.log('App is running');
+})
